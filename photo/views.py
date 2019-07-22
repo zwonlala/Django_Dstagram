40cf34f0f5387fa801f1e06ec569d1511f04f9d7
송지원 <songjiwon@songjiwon-ui-MacBookPro.local>
2019-07-22 21:09:56 +0900

    Dstagram first commit

---
+++ photo/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from .models import Photo
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.shortcuts import redirect
+
+
+def photo_list(request):
+    photos = Photo.objects.all()
+    return render(request, 'photo/list.html', {'photos':photos})
+
+
+class PhotoUploadView(CreateView):
+    model = Photo
+    fields = ['photo', 'text']
+    template_name = 'photo/upload.html'
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.id
+
+        if form.is_valid():
+            form.instance.save()
+            return redirect('/')
+        else:
+            return self.render_to_response({'form':form})
+
+
+
+class PhotoDeleteView(DeleteView):
+    model = Photo
+    success_url = '/'
+    template_name = 'photo/delete.html'
+
+
+class PhotoUpdateView(UpdateView):
+    model = Photo
+    fields = ['photo', 'text']
+    template_name = 'photo/update.html'
+
+    
